eb06ad0ddd2a84dc0e5cd2d5d7c875d9573388ed
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-19 21:43:50 +0100

    Create finding-the-average-in-a-loop.py

---
+++ loops-and-iterations/exercises/finding-the-average-in-a-loop.py
@@ -0,0 +1,9 @@
+count = 0
+sum = 0
+print('Before',count, sum)
+for value in [9, 41, 12, 3, 74, 15] :
+    count = count + 1 
+    sum = sum + value
+    print(count, sum, value)
+print('After', count, sum, sum/count)
+# An average just combines the counting and sum patterns and divides when the loop is done.
