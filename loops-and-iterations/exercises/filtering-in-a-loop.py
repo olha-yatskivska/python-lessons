cfa553f8560bd15c3c8115a91a9d19a67f26ab36
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-19 21:47:11 +0100

    Create filtering-in-a-loop.py

---
+++ loops-and-iterations/exercises/filtering-in-a-loop.py
@@ -0,0 +1,8 @@
+print('Before')
+for value in [9, 41, 1, 3, 74, 15]:
+    if value > 20:
+        print('Larger number', value)
+print('After')        
+
+
+# We use an if statement in the loop to catch / filter the values we are looking for.
