9c3ee51a7edd9fc03db6a89695f132b2bac8b8ea
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-19 21:35:54 +0100

    Create finding-the-largest-value.py

---
+++ loops-and-iterations/exercises/finding-the-largest-value.py
@@ -0,0 +1,11 @@
+largest_so_far = -1
+print('Before', largest_so_far)
+for the_num in [9, 41, 12, 3, 74, 15]:
+    if the_num > largest_so_far:
+        largest_so_far = the_num
+    print(largest_so_far, the_num)
+    
+print('After', largest_so_far)
+
+# We make a variable that contains the largest value we have seen so far. 
+# if the current number we are looking at is larger, it is the new largest value we have have seen far.
