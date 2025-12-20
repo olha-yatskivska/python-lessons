f35af62788d54d101b51bd00d7dbf5620b68ce03
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-20 21:09:27 +0100

    Update and rename looping-through-strings-with-while.py to looping-through-strings.py

--- strings/exercises/looping-through-strings.py
+++ strings/exercises/looping-through-strings.py
@@ -1,6 +1,14 @@
+# with while statement
+
 fruit = 'banana'
 index = 0 
 while index < len(fruit):
     letter = fruit[index]
     print(index, letter)
     index = index + 1
+
+
+# with for statement
+fruit = 'banana'
+for letter in fruit:
+    print(letter)
