af7c8b27c8d09167a54df8b0dfdda00a2e5e99d9
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-21 23:28:47 +0100

    Update homework-exercises-07-03.py

--- files/exercises/homework-exercises-07-03.py
+++ files/exercises/homework-exercises-07-03.py
@@ -1,9 +1,7 @@
-Exercise 3:
-
+#Exercise 3:
 #Sometimes when programmers get bored or want to have a bit of fun, they add a harmless Easter Egg to their program.
 #Modify the program that prompts the user for the file name so that it prints a funny message when the user types in the exact file name “na na boo boo”. 
 #The program should behave normally for all other files which exist and don’t exist. Here is a sample execution of the program:
-
 
 #python egg.py
 #Enter the file name: mbox.txt
