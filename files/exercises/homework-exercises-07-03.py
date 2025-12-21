ac1cd4331f5e5eacd44453e8520a195a9074ba75
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-21 23:47:55 +0100

    Update homework-exercises-07-03.py

--- files/exercises/homework-exercises-07-03.py
+++ files/exercises/homework-exercises-07-03.py
@@ -20,12 +20,12 @@ fname = input('Enter the file name: ')
 if fname == 'na na boo boo':
   print ("NA NA BOO BOO TO YOU - You have been punk'd!")
   quit()
-else: 
-  try:
-    fhand = open(fname)
-  except:
-    print('File cannot be opened:', fname)
-    quit()
+
+try:
+  fhand = open(fname)
+except:
+  print('File cannot be opened:', fname)
+  quit()
   count = 0
   for line in fhand: 
     if line.startswith('Subject:') :
