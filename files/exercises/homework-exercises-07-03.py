3a3154ac9e9e7d3bc4d3d06bd751bd703011e560
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-21 23:34:27 +0100

    Update homework-exercises-07-03.py

--- files/exercises/homework-exercises-07-03.py
+++ files/exercises/homework-exercises-07-03.py
@@ -25,8 +25,9 @@ except:
 if fname == 'na na boo boo':
   print ("NA NA BOO BOO TO YOU - You have been punk'd!")
 else: 
-count = 0
-for line in fhand: 
-  if line.startswith('Subject:') :
-    count = count + 1
+  count = 0
+  for line in fhand: 
+    if line.startswith('Subject:') :
+      count = count + 1
+      
 print('There were', count, 'subject lines in', fname)
