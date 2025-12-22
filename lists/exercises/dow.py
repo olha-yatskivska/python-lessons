12be38f2bb3343145e9fdf372358b3044451ff99
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-22 21:31:32 +0100

    Update dow.py

--- lists/exercises/dow.py
+++ lists/exercises/dow.py
@@ -3,6 +3,7 @@ han = open('mbox-short.txt')
 for line in han: 
   line = line.rstrip()
   wds = line.split()
-  if wds[0] != 'From' :
+  # guadian pattern
+  if  len(wds) < 3 or wds[0] != 'From' :
     continue
   print(wds[2])
