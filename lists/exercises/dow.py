bfde4596d1e87042a78c3653f3ee60a21c595034
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-22 21:51:04 +0100

    Update dow.py

--- lists/exercises/dow.py
+++ lists/exercises/dow.py
@@ -2,8 +2,14 @@ han = open('mbox-short.txt')
 
 for line in han: 
   line = line.rstrip()
+  # print('Line:',line)
+  # if line == '':
+    # print('Skip Blank')
+    # continue
   wds = line.split()
-  # guadian pattern
-  if  len(wds) < 3 or wds[0] != 'From' :
+  # print('Words:',wds)
+  # guardian in a compound statement
+  if len(wds) < 3 or wds[0] != 'From' :
+    # print('Ignore')
     continue
   print(wds[2])
