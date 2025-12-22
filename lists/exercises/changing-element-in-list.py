c442c70b099d289d4a1b0e9e0023353331952b4b
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-22 09:42:06 +0100

    Create changing-element-in-list.py

---
+++ lists/exercises/changing-element-in-list.py
@@ -0,0 +1,10 @@
+fruit = 'Bananna'
+#fruit[0] = 'b' - traceback. Type Error: 'str' object does not support item assigment
+x = fruit.lower()
+print(x)
+
+lotto = [2, 14, 26, 41, 63]
+print(lotto)
+lotto[2]=28
+print(lotto)
+
