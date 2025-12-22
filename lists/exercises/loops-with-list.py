4cfe29aeac09ecbfc60e19c91e71071d56a77c04
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-22 12:48:49 +0100

    Update loops-with-list.py

--- lists/exercises/loops-with-list.py
+++ lists/exercises/loops-with-list.py
@@ -1,7 +1,7 @@
 friends = ['Joseph', 'Glenn', 'Sally']
 for friend in friends :
-  print ('Happt New Year:', friend )
+  print ('Happy New Year:', friend )
 
 for i in range(len(friends)) :
   friend = friends[i]
-  print ('Happt New Year:', friend )
+  print ('Happy New Year:', friend )
