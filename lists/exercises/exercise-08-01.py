eac50af00ea122dfc271770468402038f12b2f52
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-22 23:01:12 +0100

    Update exercise-08-01.py

--- lists/exercises/exercise-08-01.py
+++ lists/exercises/exercise-08-01.py
@@ -1,13 +1,14 @@
 # Exercise 1: Write a function called chop that takes a list and modifies it, removing the first and last elements, and returns None. 
 # Then write a function called middle that takes a list and returns a new list that contains all but the first and last elements.
 
-def chop()
+def chop(t):
+  del t[0]
+
+numbers = [5, 6, 7, 8, 9]
+chop(numbers)
+print(numbers)
 
 
-chop()
 
 
-def middle()
 
-
-middle()
