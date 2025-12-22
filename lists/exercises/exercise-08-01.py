5b95b87cb46796baefd7c7503316ae5dc057163c
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-22 23:02:03 +0100

    Update exercise-08-01.py

--- lists/exercises/exercise-08-01.py
+++ lists/exercises/exercise-08-01.py
@@ -2,7 +2,7 @@
 # Then write a function called middle that takes a list and returns a new list that contains all but the first and last elements.
 
 def chop(t):
-  del t[0]
+  del t[0, -1]
 
 numbers = [5, 6, 7, 8, 9]
 chop(numbers)
