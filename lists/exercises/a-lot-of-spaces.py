12bce8f6d8e1e42ba8f8e3810a8e937d2ca79b6d
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-22 17:53:46 +0100

    Create a-lot-of-spaces.py

---
+++ lists/exercises/a-lot-of-spaces.py
@@ -0,0 +1,11 @@
+line = 'A lot               of spaces'
+etc = line.split()
+print(etc)
+
+
+line = 'first;second;third'
+thing = line.split()
+print(len(thing))
+thing = line.split(';')
+print(thing)
+print(len(thing))
