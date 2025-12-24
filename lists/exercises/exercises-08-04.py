94315768489793b68e7ac7bf695c83d60265c282
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-24 11:11:35 +0100

    Update exercises-08-04.py

--- lists/exercises/exercises-08-04.py
+++ lists/exercises/exercises-08-04.py
@@ -7,10 +7,10 @@ fh = open(fname)
 lst = list()
 for line in fh:
   words = line.split()
-  print(words)
-if word not in lst:
-  lst.append(word)
-  print(lst)
+  print(words(rstrip())
+#if word not in lst:
+ # lst.append(word)
+  #print(lst)
   # words = line.split()
   #for word in words 
 #print(line(rstrip())
