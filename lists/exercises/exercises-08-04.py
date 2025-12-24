43bb320adead7efb90137ff98f73d275d8546de3
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-24 11:04:23 +0100

    Update exercises-08-04.py

--- lists/exercises/exercises-08-04.py
+++ lists/exercises/exercises-08-04.py
@@ -4,9 +4,14 @@
 
 fname = input("Enter file name: ")
 fh = open(fname)
+lst = list()
 for line in fh:
-  words = line.split()
-  print(words)
+  word = line.split()
+   print(words)
+  lst = list()
+  if word not in lst:
+    lst.append(word)
+    print(lst)
   # words = line.split()
   #for word in words 
 #print(line(rstrip())
