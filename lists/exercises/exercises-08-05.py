9a34bef91395ffed9e87b528d3864b95fbeaf646
Olha Yatskivska <o.mayovetskaya@gmail.com>
2025-12-25 14:21:02 +0100

    Update exercises-08-05.py

--- lists/exercises/exercises-08-05.py
+++ lists/exercises/exercises-08-05.py
@@ -11,10 +11,7 @@ fname = input("Enter file name: ")
 if len(fname) < 1:
     fname = "mbox-short.txt"
 fh = open(fname)
-count = 0
-while True:
-        line[0] = 'From'              
-        count = count + 1
+
 for line in fh:
     line = line.rstrip()
     words = line.split()
@@ -24,6 +21,10 @@ for line in fh:
             #count.append(value)
     #print(line)
     print(words[1])
+    count = 0
+while True:
+        words[0] = 'From'              
+        count = count + 1g
 print("There were", count, "lines in the file with From as the first word")      
 
  
