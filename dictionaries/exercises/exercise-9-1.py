d0ea64748ae7a4b5c303eca9d68f490d78a17997
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-04 20:46:32 +0100

    Create exercise-9-1.py

---
+++ dictionaries/exercises/exercise-9-1.py
@@ -0,0 +1,14 @@
+# Write a program that reads the words in words.txt and stores them as keys in a dictionary
+# It doesn’t matter what the values are
+# Then you can use the in operator as a fast way to check whether a string is in the dictionary
+
+
+name = "mbox-short.txt"
+handle = open(name)
+
+programs = dict()
+ for line in handle : 
+   words = line.split()
+   for word in words :
+     counts[words] = counts.get(word,0) + 1
+     
