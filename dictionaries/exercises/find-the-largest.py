27437310d68938037ff56b1d19f394145562968f
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-04 20:45:24 +0100

    Update find-the-largest.py

--- dictionaries/exercises/find-the-largest.py
+++ dictionaries/exercises/find-the-largest.py
@@ -5,7 +5,7 @@ counts = dict()
 for line in handle:
   words = line.split()
   for word in words:
-    countints[words] = counts.get(word,0) + 1
+    counts[words] = counts.get(word,0) + 1
 
 bigcount = None
 bigword = None
