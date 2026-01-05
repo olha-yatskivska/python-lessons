3f59d2a915dbd4a1d855be4daf173fb23800edef
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-05 11:05:45 +0100

    Update exercises-9-2.py

--- dictionaries/exercises/exercises-9-2.py
+++ dictionaries/exercises/exercises-9-2.py
@@ -1,7 +1,7 @@
 # Exercise 2: Write a program that categorizes each mail message by which day of the week the commit was done. 
 # To do this look for lines that start with “From”, then look for the third word and keep a running count of each of the days of the week. 
 # At the end of the program print out the contents of your dictionary (order does not matter).
-
+counts = dict()
 name = input("Enter file:")
 fhandle = open(name)
 for line in fhandle :
@@ -9,14 +9,10 @@ for line in fhandle :
   words = line.split()
   if len(words) < 3 or words[0] != 'From':
     continue
-    print(words[3]) 
-    
-
+    print(words[1]) 
       
-#counts = dict()
-
 #for words in line :
  # words = line.split()
   #for word in words :
-   # word = counts[word] = counts.get(word,0) + 1
-# print('Counts', counts)
+   # word = counts[word[3]] = counts.get(word[3],0) + 1
+ #print('Counts', counts)
