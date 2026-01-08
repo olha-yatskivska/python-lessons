17827b3342f097e16ea9a4fa7fea1f9a9852aad3
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-08 13:43:01 +0100

    Update exercises-08-05.py

--- lists/exercises/exercises-08-05.py
+++ lists/exercises/exercises-08-05.py
@@ -24,14 +24,3 @@ for line in fh:
 print("There were", count, "lines in the file with From as the first word")      
 
  
-#
-
-#fname = input("Enter file name: ")
-#if len(fname) < 1:
- #   fname = "mbox-short.txt"
-
-#fh = open(fname)
-#count = 0
-
-#print("There were", count, "lines in the file with From as the first word")
-
