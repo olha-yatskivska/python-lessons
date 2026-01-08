81c07bea3c569f069e6b2961402fcbf87de3cc00
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-08 13:49:36 +0100

    Update exercise-09.py

--- dictionaries/exercises/exercise-09.py
+++ dictionaries/exercises/exercise-09.py
@@ -1,8 +1,10 @@
 fname = input('Enter file: ')
 if len(fname) < 1 : fname = 'clown.txt'
 fhand = open(fname)
-for line in fhandle :
+for line in fhand :
   line = line.rstrip()
-  print(line)
+  # print(line)
   
+  wds = line.split()
+  print(wds)
 #print(fname, len(fname))
