1700b5cc3337f0a3a0dccdcd145ab65be480a330
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-12 22:32:08 +0100

    Create list-comprehension.py

---
+++ tuples/exercises/list-comprehension.py
@@ -0,0 +1,12 @@
+list_of_ints_in_strings = ['42', '65', '12']
+list_of_ints = []
+for x in list_of_ints_in_strings:
+    list_of_ints.append(int(x))
+
+print(sum(list_of_ints))
+
+# the above code can be written in a more compact manner:
+
+list_of_ints_in_strings = ['42', '65', '12']
+list_of_ints = [ int(x) for x in list_of_ints_in_strings ]
+print(sum(list_of_ints))
