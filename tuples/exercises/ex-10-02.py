cdcd34452ba185b95d46e61e37c2a426fa8a9e2c
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-13 12:27:23 +0100

    Update ex-10-02.py

--- tuples/exercises/ex-10-02.py
+++ tuples/exercises/ex-10-02.py
@@ -1,4 +1,4 @@
-
+#Exercise 2: This program counts the distribution of the hour of the day for each of the messages. You can pull the hour from the “From” line by finding the time string and then splitting that string into parts using the colon character. Once you have accumulated the counts for each hour, print out the counts, one per line, sorted by hour as shown below.
 
 counts = dict()
 name = input("Enter file:")
@@ -16,20 +16,23 @@ for line in fhandle:
   if len(words) < 3 or words[0] != 'From':
     continue
   
-  t = words[5] 
-  num  = t.find(':') 
-  h = t[:num]
+  t = words[5].split(':')
+    
+  
+    
+  #num  = t.find(':') 
+  #h = t[:num]
   
   counts[h] = counts.get(h,0) + 1
-  #print(mail)
+  print(mail)
 
-newlist = list()
-for k, v in counts.items() :
-  tup = (k,v)
-  newlist.append(tup)
+#newlist = list()
+#for k, v in counts.items() :
+ # tup = (k,v)
+ # newlist.append(tup)
  
-hours = sorted(newlist)
+#hours = sorted(newlist)
 
-print(hours[1], hours[0])
+#print(hours[1], hours[0])
 
 
