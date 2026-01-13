d1d7d55ed9a89ba70ceba915cecae3234a8286a0
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-13 12:38:22 +0100

    Update ex-10-02.py

--- tuples/exercises/ex-10-02.py
+++ tuples/exercises/ex-10-02.py
@@ -15,10 +15,11 @@ for line in fhandle:
   words = line.split()
   if len(words) < 3 or words[0] != 'From':
     continue
-      for word in words:
-          t = words[5].split(':')
-          h = t[0]
-          counts[h] = counts.get(h,0) + 1
+      
+    for word in words:
+        t = words[5].split(':')
+        h = t[0]
+        counts[h] = counts.get(h,0) + 1
        
  print(counts)
 
