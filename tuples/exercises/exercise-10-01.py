a75047ed4b34140c862cc66cf828699f3b14da5f
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-13 09:27:11 +0100

    Create exercise-10-01.py

---
+++ tuples/exercises/exercise-10-01.py
@@ -0,0 +1,28 @@
+#Exercise 1: Revise a previous program as follows: Read and parse the “From” lines and pull 
+#out the addresses from the line. Count the number of messages from each person using a dictionary.
+
+#After all the data has been read, print the person with the most commits by creating a list of 
+#(count, email) tuples from the dictionary. Then sort the list in reverse order and print out the
+#person who has the most commits.
+
+counts = dict()
+name = input("Enter file:")
+try:
+  fhandle = open(name)
+except:
+  print("File cannot be opened:", name)
+  quit()
+
+for line in fhandle:
+  line = line.rstrip()
+  words = line.split()
+  if len(words) < 3 or words[0] != 'From':
+    continue
+  
+  email = words[1] 
+ 
+  
+  counts[email] = counts.get(email,0) + 1
+  #print(mail)
+  
+print(counts)
