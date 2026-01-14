36608cfa357e0b1de9f9b54d813ab3b6217c1707
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-14 21:14:48 +0100

    Update extracting-host-name.py

--- regular-expressions/exercises/extracting-host-name.py
+++ regular-expressions/exercises/extracting-host-name.py
@@ -2,7 +2,7 @@
 data = 'From stephen.marquard@utc.ac.za Sat Jan 5 09:14:16 2008'
 atpos = data.find('@')
 print(atpos)
-sppos = data.find('',atpos)
+sppos = data.find(' ',atpos)
 print(sppos)
 host = data[atpos+1 : sppos]
 print(host)
