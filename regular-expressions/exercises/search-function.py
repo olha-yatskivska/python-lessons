041f52cb9a5ed83e61aa9c1e3ef03400b67e05fb
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-14 14:54:04 +0100

    Update search-function.py

--- regular-expressions/exercises/search-function.py
+++ regular-expressions/exercises/search-function.py
@@ -5,3 +5,11 @@ for line in hand:
     line = line.rstrip()
     if re.search('From:', line):
         print(line)
+
+# Search for lines that start with 'From'
+import re
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    if re.search('^From:', line):
+        print(line)
