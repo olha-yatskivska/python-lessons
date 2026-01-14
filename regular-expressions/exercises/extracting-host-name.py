ea087843a1338815135b176ca45c3a1d84220acd
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-14 21:21:02 +0100

    Update extracting-host-name.py

--- regular-expressions/exercises/extracting-host-name.py
+++ regular-expressions/exercises/extracting-host-name.py
@@ -11,8 +11,8 @@ print(host)
 
 data = 'From stephen.marquard@utc.ac.za Sat Jan 5 09:14:16 2008'
 
-for words in data:
-  words = line.split()
+
+  words = data.split()
   email=words[1]
   pieces = email.split('@')
 print(pieces[1])
