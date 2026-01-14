e6ccd5488d989ed69abbceac400032d80eeacfc4
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-14 21:10:06 +0100

    Create extracting-host-name.py

---
+++ regular-expressions/exercises/extracting-host-name.py
@@ -0,0 +1,8 @@
+# using find and string slicing
+data = 'From stephen.marquard@utc.ac.za Sat Jan 5 09:14:16 2008'
+atpos == data.find('@')
+print(atpos)
+sppos = data.find('',atpos)
+print(sppos)
+host = data[atpos+1 : sspos]
+print(host)
