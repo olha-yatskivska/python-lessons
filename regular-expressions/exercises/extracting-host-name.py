bbfb2f4ae918533722bce06c6e30d6bd121e9ccd
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-14 21:16:47 +0100

    Update extracting-host-name.py

--- regular-expressions/exercises/extracting-host-name.py
+++ regular-expressions/exercises/extracting-host-name.py
@@ -6,3 +6,10 @@ sppos = data.find(' ',atpos)
 print(sppos)
 host = data[atpos+1 : sppos]
 print(host)
+
+# the double split pattern
+data = 'From stephen.marquard@utc.ac.za Sat Jan 5 09:14:16 2008'
+words = line.split()
+email=words[1]
+pieces = email.split('@')
+print(pieces[1])
