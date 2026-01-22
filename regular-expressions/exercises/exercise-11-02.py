ec98ec7fe864b5eafaa3fe69c166ce1a693eae6c
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-22 22:25:11 +0100

    Create exercise-11-02.py

---
+++ regular-expressions/exercises/exercise-11-02.py
@@ -0,0 +1,23 @@
+#Exercise 2: Write a program to look for lines of the form:
+#New Revision: 39772
+#Extract the number from each of the lines using a regular expression and the findall() method. 
+#Compute the average of the numbers and print out the average as an integer.
+
+import re
+
+numlist = list()
+name = input('Enter file name:')
+try: 
+  fhand = open('mbox-short.txt')
+except:
+  print("File cannot be opened:", name)
+  quit()
+
+for line in fhand:
+  line = line.rstrip()
+  numbers = re.findall('^New Revision: ([0-9]+)', line)
+  if len(numbers) !=1 : continue
+  num = int(numbers[0])
+  numlist.append(num)
+  average = sum(numlist)/len(numlist)
+print(average)
