70064b91227125ae821949fca4f5fbcf156df459
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-22 21:48:31 +0100

    Update exercise-11-01.py

--- regular-expressions/exercises/exercise-11-01.py
+++ regular-expressions/exercises/exercise-11-01.py
@@ -10,7 +10,7 @@ lst = list()
 
 for line in fhand: 
     line = line.rstrip()
-    mtch = re.findall ('([{regex}])', line)
+    mtch = re.findall ('({regex})', line)
     if len(mtch) > 0:
         count = count + 1 
         #for n in mtch:
