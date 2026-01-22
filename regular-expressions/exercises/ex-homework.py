a7eaed2dfb0c12447a603503ec1df70ab682103a
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-22 10:00:06 +0100

    Create ex-homework.py

---
+++ regular-expressions/exercises/ex-homework.py
@@ -0,0 +1,18 @@
+import re
+
+name = input ("Enter file:")
+try:
+  fhandle = open(name)
+except: 
+  print("Print cannot be opened:", name)
+  quit()
+numlist = list()
+for line in fhandle:
+  line = line.rstrip()
+  numbers = re.findall('([0-9]+)' , line)
+  num = int(numbers[0])
+  numlist.append(num)
+
+print(numlist)
+
+
