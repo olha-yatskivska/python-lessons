a67e489f434268ef40363fd0cd5142b40247f1e7
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-22 10:28:16 +0100

    Update ex-homework.py

--- regular-expressions/exercises/ex-homework.py
+++ regular-expressions/exercises/ex-homework.py
@@ -10,9 +10,9 @@ numlist = list()
 for line in fhandle:
   line = line.rstrip()
   numbers = re.findall('([0-9]+)' , line)
-  if len(numbers) > 0: continue
-  num = int(numbers[0])
-  numlist.append(num)
+  if len(numbers) > 0: 
+    num = int(numbers[0])
+    numlist.append(num)
 
 print(sum(numlist))
 
