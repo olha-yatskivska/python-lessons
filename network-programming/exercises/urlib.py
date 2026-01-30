4e6333a5c3ceadfde59dfddc32db43f671ae74dd
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-30 23:19:18 +0100

    Update urlib.py

--- network-programming/exercises/urlib.py
+++ network-programming/exercises/urlib.py
@@ -4,6 +4,7 @@ fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 
 counts = dict()
 for line in fhand:
+    #print(line.decode().strip())
     words = line.decode().split()
     for word in words:
         counts[word] = counts.get(word, 0) + 1
