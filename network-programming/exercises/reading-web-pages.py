f423f39d86536589025495a80445901116461d90
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-01-30 23:28:31 +0100

    Update reading-web-pages.py

--- network-programming/exercises/reading-web-pages.py
+++ network-programming/exercises/reading-web-pages.py
@@ -1,4 +1,4 @@
-import urllib.request, urllibparse, urllib.error
+import urllib.request, urllib parse, urllib.error
 
 fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
 for line in fhand:
