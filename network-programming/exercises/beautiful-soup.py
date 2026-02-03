dd6d30f39f5861b22017d5e13f796cd3278067bb
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-03 14:35:13 +0100

    Update beautiful-soup.py

--- network-programming/exercises/beautiful-soup.py
+++ network-programming/exercises/beautiful-soup.py
@@ -1,4 +1,5 @@
-import urllib.request, urllib.parse, urllib.error from bs4 import BeautifulSoup
+import urllib.request, urllib.parse, urllib.error 
+from bs4 import BeautifulSoup
 
 url = input('Enter - ')
 html = urllib.request.urlopen(url).read()
