aa5e4b28679952e41b3daae39e43ab1ab815b801
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-03 14:32:34 +0100

    Create beautiful-soup.py

---
+++ network-programming/exercises/beautiful-soup.py
@@ -0,0 +1,10 @@
+import urllib.request, urllib.parse, urllib.error from bs4 import BeautifulSoup
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url).read()
+soup = BeatifulSoup(html, 'html.parser')
+
+# Retrievw all of the anchor tags
+tags = soap('a')
+for tag in tags:
+  print(tag.get('href', None))
