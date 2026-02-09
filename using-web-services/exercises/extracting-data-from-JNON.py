b1029a224d81647a56fc8f783ba349acaf6debb6
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-09 13:38:17 +0100

    Create extracting-data-from-JNON.py

---
+++ using-web-services/exercises/extracting-data-from-JNON.py
@@ -0,0 +1,19 @@
+import urllib.request
+import json
+
+url = input('Enter location: ')
+if len(url) < 1 : 
+    url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
+
+print('Retrieving', url)
+uh = urllib.request.urlopen(url)
+data = uh.read()
+
+info = json.loads(uh)
+print('User count:', len(info))
+
+nums = list()
+for item in info:
+    print('count', item['count'])
+    print('Id', item['id'])
+    print('Attribute', item['x'])
