c608c71fc41bb759b43b36a2923b0a823f447902
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-09 14:04:07 +0100

    Update extracting-data-from-JSON.py

--- using-web-services/exercises/extracting-data-from-JSON.py
+++ using-web-services/exercises/extracting-data-from-JSON.py
@@ -8,9 +8,10 @@ if len(url) < 1 :
 print('Retrieving', url)
 uh = urllib.request.urlopen(url)
 data = uh.read()
-
+print('User count:', len(data))
 info = json.loads(data)
-print('User count:', len(info))
+
+
 
 #nums = list()
 #for item in info:
