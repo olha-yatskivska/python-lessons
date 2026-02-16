e16bc68ecf213ff91a1a2e277994e47f63d0296e
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-16 22:26:35 +0100

    Update roster-homework.py

--- databases/exercises/roster-homework/roster-homework.py
+++ databases/exercises/roster-homework/roster-homework.py
@@ -33,7 +33,7 @@ if len(fname) < 1:
     fname = 'roster_data.json'
 
 #   [ "Charley", "si110", 1 ],
-#   [ "Mea", "si110", 0 ],
+#   [ "Mea", "si110", 0 ], 
 
 str_data = open(fname).read()
 json_data = json.loads(str_data)
