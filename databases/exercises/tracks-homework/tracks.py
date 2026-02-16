3cd2af97e66421b80995a3ab2b6aadedeb385165
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-16 22:50:18 +0100

    Update tracks.py

--- databases/exercises/tracks-homework/tracks.py
+++ databases/exercises/tracks-homework/tracks.py
@@ -6,24 +6,31 @@ cur = conn.cursor()
 # Make some fresh tables using executescript()
 cur.executescript('''
 DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Genre;
 DROP TABLE IF EXISTS Album;
 DROP TABLE IF EXISTS Track;
 
 CREATE TABLE Artist (
-    id  INTEGER PRIMARY KEY,
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Genre (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name    TEXT UNIQUE
 );
 
 CREATE TABLE Album (
-    id  INTEGER PRIMARY KEY,
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     artist_id  INTEGER,
     title   TEXT UNIQUE
 );
 
 CREATE TABLE Track (
-    id  INTEGER PRIMARY KEY,
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     title TEXT  UNIQUE,
     album_id  INTEGER,
+    genre_id INTEGER,
     len INTEGER, rating INTEGER, count INTEGER
 );
 ''')
@@ -36,7 +43,7 @@ handle = open('tracks.csv')
 for line in handle:
     line = line.strip();
     pieces = line.split(',')
-    if len(pieces) < 6 : continue
+    if len(pieces) < 7 : continue
 
     name = pieces[0]
     artist = pieces[1]
@@ -44,13 +51,19 @@ for line in handle:
     count = pieces[3]
     rating = pieces[4]
     length = pieces[5]
+    genre = pieces[6]
 
-    print(name, artist, album, count, rating, length)
+    print(name, artist, album, count, rating, length, genre)
 
     cur.execute('''INSERT OR IGNORE INTO Artist (name) 
         VALUES ( ? )''', ( artist, ) )
     cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
     artist_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Genre (name) 
+        VALUES ( ? )''', ( genre, ) )
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+    genre_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
         VALUES ( ?, ? )''', ( album, artist_id ) )
@@ -58,8 +71,8 @@ for line in handle:
     album_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, len, rating, count) 
-        VALUES ( ?, ?, ?, ?, ? )''', 
-        ( name, album_id, length, rating, count ) )
+        (title, album_id, genre_id, len, rating, count) 
+        VALUES ( ?, ?, ?, ?, ?, ? )''', 
+        ( name, album_id, genre_id, length, rating, count ) )
 
     conn.commit()
