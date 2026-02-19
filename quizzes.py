259ee5cc50d57fe5c5bbd7e3c156e0ac2bcff6ec
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-19 14:33:27 +0100

    Update quizzes.py

--- quizzes.py
+++ quizzes.py
@@ -1,3 +1,8 @@
-li = [1, 2, 3, 4] 
-li.append([5,6,7,8]) 
-print(li) 
+tup = {} 
+tup[(1,2,4)] = 8
+tup[(4,2,1)] = 10
+tup[(1,2)] = 12
+sum1 = 0
+for k in tup: 
+	sum1 += tup[k] 
+print(len(tup) + sum1) 
