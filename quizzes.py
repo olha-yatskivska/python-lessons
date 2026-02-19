08975392e011c5041cf0b4059b3e90fa6db0791f
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-19 21:51:52 +0100

    Update quizzes.py

--- quizzes.py
+++ quizzes.py
@@ -1,6 +1,11 @@
-d = {'GFG' : 1, 
-		'Facebook' : 2, 
-		'Google' : 3
-		} 
-for (key, values) in d.items(): 
-	print(key, values, end = " ")
+d1 = {'Google' : 1, 
+			'Facebook' : 2, 
+			'Microsoft' : 3
+			} 
+d2 = {'GFG' : 1, 
+			'Microsoft' : 2, 
+			'Youtube' : 3
+			} 
+d1.update(d2); 
+for key, values in d1.items(): 
+	print(key, values , end=" ") 
