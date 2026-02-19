b493099ddf72925c07f703891171a33940efc95e
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-19 14:39:33 +0100

    Update quizzes.py

--- quizzes.py
+++ quizzes.py
@@ -1,5 +1,9 @@
-s = 'geeks' 
-a, b, c, d, e = s 
-b = c = '*'
-s = (a, b, c, d, e) 
-print(s) 
+li = [3, 1, 2, 4] 
+tup = ('A', 'b', 'c', 'd') 
+li.sort() 
+counter = 0
+for x in tup: 
+	li[counter] += int(x) 
+	counter += 1
+	break
+print(li) 
