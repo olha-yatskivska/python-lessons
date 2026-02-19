b60beb3904ef25404551b0864a6d6ea8064bd411
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-19 14:05:42 +0100

    Update quizzes.py

--- quizzes.py
+++ quizzes.py
@@ -1,6 +1,15 @@
-def gfg(x,li=[]): 
-	for i in range(x): 
-		li.append(i*i) 
-	print(li) 
+a = ['Learn', 'Quiz', 'Practice', 'Contribute'] 
+b = a 
+c = a[:] 
 
-gfg(3,[3,2,1])
+b[0] = 'Code'
+c[1] = 'Mcq'
+
+count = 0
+for c in (a, b, c): 
+	if c[0] == 'Code': 
+		count += 1
+	if c[1] == 'Mcq': 
+		count += 10
+
+print (count) 
