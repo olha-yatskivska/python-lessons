cae759ed17eb38bd17044025dd1429e1aeb02fc6
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-19 12:22:01 +0100

    Update quizzes.py

--- quizzes.py
+++ quizzes.py
@@ -1,7 +1,4 @@
-count = 0
-while count < 5:
-   print("Count:", count)
-   count += 1
-   if count == 3:
-       continue
-   print("After Continue")
+num = 2
+while num < 10:
+   print(num, end=" ")
+   num **= 2
