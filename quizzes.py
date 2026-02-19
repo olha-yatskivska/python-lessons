ef0b575fcda49c367b0c972aae3833cd4330a1cb
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-19 14:07:48 +0100

    Update quizzes.py

--- quizzes.py
+++ quizzes.py
@@ -1,15 +1,5 @@
-a = ['Learn', 'Quiz', 'Practice', 'Contribute'] 
-b = a 
-c = a[:] 
-
-b[0] = 'Code'
-c[1] = 'Mcq'
-
-count = 0
-for c in (a, b, c): 
-	if c[0] == 'Code': 
-		count += 1
-	if c[1] == 'Mcq': 
-		count += 10
-
-print (count) 
+def addToList(a): 
+	a += [10] 
+b = [10, 20, 30, 40] 
+addToList(b) 
+print (len(b)) 
