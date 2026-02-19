0f80d2f6c798a440c01e5313d6c6dc7e6321a805
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-19 13:27:27 +0100

    Update quizzes.py

--- quizzes.py
+++ quizzes.py
@@ -1,9 +1,5 @@
-num = 10
-while num > 0:
-   if num % 2 == 0:
-       print(num, end=" ")
-   num -= 1
- 
-   
-   
- 
+nameList = ['Harsh', 'Pratik', 'Bob', 'Dhruv'] 
+
+pos = nameList.index("GeeksforGeeks") 
+
+print (pos * 3)
