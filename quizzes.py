9e3e1380003a08aec10b42a84fc0e2998cde5dda
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-19 13:57:34 +0100

    Update quizzes.py

--- quizzes.py
+++ quizzes.py
@@ -1,10 +1,2 @@
-a = [10, 20, 30, 40, 50] 
-b = [1, 2, 3, 4, 5] 
-
-
-subtracted = list()
-for a, b in zip(a, b):
-    item = a -b
-    subtracted.append(item)
-
-print(subtracted)
+a = ['python', 'learning', '@', 'Geeks', 'for', 'Geeks'] 
+print(a[0:6:2])
