2b0dad0314c763be6ab2fb36b5df1d80d3013c7d
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-19 13:33:48 +0100

    Update quizzes.py

--- quizzes.py
+++ quizzes.py
@@ -1,10 +1,2 @@
-def REVERSE(a): 
-	a.reverse() 
-	return(a) 
-def YKNJS(a): 
-	b = [] 
-	b.extend(REVERSE(a)) 
-	print(b) 
-
-a = [1, 3.1, 5.31, 7.531] 
-YKNJS(a) 
+li = [1, 3, 5, 7, 9] 
+print(li.pop(-3), end = ' ') 
