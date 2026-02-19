b31e367aab7554be414303e4e4ddec4e9bd59711
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-19 12:01:57 +0100

    Update quizzes.py

--- quizzes.py
+++ quizzes.py
@@ -1,4 +1,7 @@
-total = 0
-for num in range(1, 6):
-   total += num
-print(total)
+count = 0
+while count < 5:
+   print("Count:", count)
+   count += 1
+   if count == 3:
+       continue
+   print("After Continue")
