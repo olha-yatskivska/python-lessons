a4274b8b6f963de42e0ed0400b0eafdbcbbc8e4f
Olha Yatskivska <o.mayovetskaya@gmail.com>
2026-02-19 21:34:28 +0100

    Update quizzes.py

--- quizzes.py
+++ quizzes.py
@@ -1,9 +1,5 @@
-import sys 
-tup = tuple() 
-print(sys.getsizeof(tup), end = " ") 
-tup = (1, 2) 
-print(sys.getsizeof(tup), end = " ") 
-tup = (1, 3, (4, 5)) 
-print(sys.getsizeof(tup), end = " ") 
-tup = (1, 2, 3, 4, 5, [3, 4], 'p', '8', 9.777, (1, 3)) 
-print(sys.getsizeof(tup)) 
+d = dict() 
+for x in enumerate(range(2)): 
+	d[x[0]] = x[1] 
+	d[x[1]+7] = x[0] 
+print(d) 
